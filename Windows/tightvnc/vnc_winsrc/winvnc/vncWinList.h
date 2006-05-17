75ef8ba0cc071dcb65e638174b8d00bc3460e367
(no author) <(no author)@4983599a-be78-4323-97cb-837624fd53a2>
2006-05-17 15:22:08 +0000

    This commit was manufactured by cvs2svn to create tag 'pre_loop_optimize'.

---
+++ Windows/tightvnc/vnc_winsrc/winvnc/vncWinList.h
@@ -0,0 +1,43 @@
+//
+//  Copyright (c) 2002 Alkit Communications
+//
+//
+// vncWinList
+//
+// Object implementing the Window selection dialog for WinVNC.
+//
+
+class vncWinList;
+
+#if (!defined(_WINVNC_VNCWINLIST))
+#define _WINVNC_VNCWINLIST
+
+// Includes
+#include "stdhdrs.h"
+#include "sharedapp.h"
+
+// The vncWinList class itself
+class vncWinList
+{
+public:
+	// Constructor/destructor
+	vncWinList(SharedAppVnc *_shapp);
+	~vncWinList();
+
+	// Initialisation
+	BOOL Init();
+
+	// The dialog box window proc
+	static BOOL CALLBACK DialogProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+	// General
+	void Show(BOOL show);
+
+	// Implementation
+	BOOL m_dlgvisible;
+
+private:
+	SharedAppVnc *m_shapp;
+};
+
+#endif // _WINVNC_VNCPROPERTIES
